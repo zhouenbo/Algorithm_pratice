ed92c9752370ee4286ce14632cd66911981caf5d
Enbo Zhou <enbozhou@126.com>
2021-03-22 20:31:23 -0700

    add a question

--- Union_find.py
+++ Union_find.py
@@ -17,7 +17,6 @@ class DSU:
     def union(self, x, y):
         self.p[self.find(x)] = self.find(y)
 
-
 def largestIsland(mat):
     row = len(mat)
     col = len(mat[0])
@@ -76,3 +75,106 @@ if __name__ == "__main__":
     ]
 
     print(largestIsland(grid))
+
+
+
+
+
+
+# 434. Number of Islands II
+# 中文English
+# Given a n,m which means the row and column of the 2D matrix and an array of pair A( size k). Originally, the 2D matrix is all 0 which means there is only sea in the matrix. The list pair has k operator and each operator has two integer A[i].x, A[i].y means that you can change the grid matrix[A[i].x][A[i].y] from sea to island. Return how many island are there in the matrix after each operator.
+#
+# Example
+# Example 1:
+#
+# Input: n = 4, m = 5, A = [[1,1],[0,1],[3,3],[3,4]]
+# Output: [1,1,2,2]
+# Explanation:
+# 0.  00000
+#     00000
+#     00000
+#     00000
+# 1.  00000
+#     01000
+#     00000
+#     00000
+# 2.  01000
+#     01000
+#     00000
+#     00000
+# 3.  01000
+#     01000
+#     00000
+#     00010
+# 4.  01000
+#     01000
+#     00000
+#     00011
+# Example 2:
+#
+# Input: n = 3, m = 3, A = [[0,0],[0,1],[2,2],[2,1]]
+# Output: [1,1,2,2]
+# Notice
+# 0 is represented as the sea, 1 is represented as the island. If two 1 is adjacent, we consider them in the same island. We only consider up/down/left/right adjacent.
+
+
+"""
+Definition for a point.
+class Point:
+    def __init__(self, a=0, b=0):
+        self.x = a
+        self.y = b
+"""
+
+
+class DSU:
+    def __init__(self, n):
+        self.p = list(range(n))
+
+    def find(self, x):
+        if x != self.p[x]:
+            self.p[x] = self.find(self.p[x])
+        return self.p[x]
+
+    def union(self, x, y):
+        self.p[self.find(x)] = self.find(y)
+
+
+class Solution:
+    """
+    @param n: An integer
+    @param m: An integer
+    @param operators: an array of point
+    @return: an integer array
+    """
+
+    def numIslands2(self, n, m, operators):
+        # write your code here
+        if not operators:
+            return []
+
+        dsu = DSU(len(operators))
+        visited = {(operators[0].x, operators[0].y): 0}
+        res = [1]
+
+        for i in range(1, len(operators)):
+            if (operators[i].x, operators[i].y) not in visited:
+                count = set()
+                if (operators[i].x - 1, operators[i].y) in visited:
+                    count.add(dsu.find(visited[(operators[i].x - 1, operators[i].y)]))
+                    dsu.union(i, visited[(operators[i].x - 1, operators[i].y)])
+                if (operators[i].x + 1, operators[i].y) in visited:
+                    count.add(dsu.find(visited[(operators[i].x + 1, operators[i].y)]))
+                    dsu.union(i, visited[(operators[i].x + 1, operators[i].y)])
+                if (operators[i].x, operators[i].y - 1) in visited:
+                    count.add(dsu.find(visited[(operators[i].x, operators[i].y - 1)]))
+                    dsu.union(i, visited[(operators[i].x, operators[i].y - 1)])
+                if (operators[i].x, operators[i].y + 1) in visited:
+                    count.add(dsu.find(visited[(operators[i].x, operators[i].y + 1)]))
+                    dsu.union(i, visited[(operators[i].x, operators[i].y + 1)])
+                visited[(operators[i].x, operators[i].y)] = i
+                res.append(res[-1] - len(count) + 1)
+            else:
+                res.append(res[-1])
+        return res
--- pratice.py
+++ pratice.py
@@ -3,6 +3,42 @@
 
 __author__ = "Enbo Zhou"
 
+import numpy as np
+
 if __name__ == "__main__":
-    a = [0.1,0.2,0.3,0.1,0.15,0.15]
-    
+    a = [[1, 2, 3, 4, 5], [2, 3, 4, 5, 6], [3, 4, 5, 6, 7]]
+    a = np.array(a)
+    print(a)
+    # print(a)
+    # u, s, vh = np.linalg.svd(a)
+    # print(u.shape, s.shape, vh.shape)
+    #
+    # print(np.diag(s))
+    #
+    # s = np.hstack((np.diag(s),np.zeros((3,2))))
+    # print(s)
+    # print(np.dot(np.dot(u,s),vh))
+    #
+
+    b = np.ones((3,5))
+    print(b)
+
+    # c = np.vstack((b,a))
+    # print(c)
+    #
+    # d = a*b
+    # print(d)
+
+    e = np.dot(a,b.T)
+    print(e)
+
+    f = np.mean(a,0)
+    print(f)
+
+    g = np.array(a,dtype=np.float)
+    for i in range(len(g)):
+        g[i] -= f
+    print(g)
+    print(a)
+
+    print(a.ndim)
